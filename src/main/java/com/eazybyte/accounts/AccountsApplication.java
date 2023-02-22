358c9b2ab16c0b91bdba44097159cfde88433c11
Abhishek Sharma <hereisabhishek.sharma@gmail.com>
2023-02-22 20:59:31 +0530

    first commit message

---
+++ src/main/java/com/eazybyte/accounts/AccountsApplication.java
@@ -0,0 +1,19 @@
+package com.eazybyte.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScans({ @ComponentScan("com.eazybyte.accounts.controller")})
+@EnableJpaRepositories("com.eazybyte.accounts.repository")
+@EntityScan("com.eazybyte.accounts.model")
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+}
